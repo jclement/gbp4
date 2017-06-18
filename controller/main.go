41afb3c3110b489d53cd5f2946aba796e7c1187e
Jeff Clement <jeffrey.clement@gmail.com>
2017-06-18 10:08:48 -0600

    Working controller

--- controller/main.go
+++ controller/main.go
@@ -32,6 +32,7 @@ const (
 	DoorClosed
 	DoorOpening
 	DoorClosing
+	DoorUnknown
 )
 
 func (state DoorState) String() string {
@@ -43,48 +44,44 @@ func (state DoorState) String() string {
 	case DoorOpening:
 		return "Opening"
 	case DoorClosing:
-		return "Closed"
+		return "Closing"
 	}
 	return "Unknown"
 }
 
 // GarageController handles door state and messaging to MQTT
 type GarageController struct {
-	currentDoorState DoorState
-	working        bool
-	client         mqtt.Client
-	config         GarageControllerConfig
-	log            *logging.Logger
-	workingChannel chan bool      // channel for communicating if door is still moving
-	statusChannel  chan DoorState // channel for communicating current status of door
-	pinStatus rpio.Pin
-	pinControl rpio.Pin
+	currentDoorState    DoorState
+	movingStatus        bool
+	client              mqtt.Client
+	config              GarageControllerConfig
+	log                 *logging.Logger
+	movingStatusChannel chan bool      // channel for communicating if door is still movingStatus
+	statusChannel       chan DoorState // channel for communicating current status of door
+	pinStatus           rpio.Pin
+	pinControl          rpio.Pin
 }
 
 // NewGarageController creates a new Garage Controller from a configuration and logging object
 func NewGarageController(config GarageControllerConfig, log *logging.Logger) *GarageController {
 
-	if err := rpio.Open(); err != nil {
-		panic(err)
-	}
-  	defer rpio.Close()
 
 	pinStatus := rpio.Pin(config.PinStatus)
-	pinStatus.Mode(rpio.Output)
+	pinStatus.Mode(rpio.Input)
+	pinStatus.Pull(rpio.PullDown)
 
 	pinControl := rpio.Pin(config.PinControl)
-	pinControl.Mode(rpio.Input)
-	pinControl.Pull(rpio.PullDown)
+	pinControl.Mode(rpio.Output)
 
 	controller := GarageController{
-		config:           config,
-		working:          false,
-		log:              log,
-		currentDoorState: DoorClosed,
-		workingChannel:   make(chan bool),
-		statusChannel:    make(chan DoorState),
-		pinControl: pinControl,
-		pinStatus: pinStatus,
+		config:              config,
+		movingStatus:        false,
+		log:                 log,
+		currentDoorState:    DoorUnknown,
+		movingStatusChannel: make(chan bool),
+		statusChannel:       make(chan DoorState),
+		pinControl:          pinControl,
+		pinStatus:           pinStatus,
 	}
 
 	clientOptions := mqtt.NewClientOptions()
@@ -95,7 +92,7 @@ func NewGarageController(config GarageControllerConfig, log *logging.Logger) *Ga
 	clientOptions.SetAutoReconnect(true)
 	clientOptions.SetWill(config.MQTTTopicPresence, "GBP-OFFLINE", 0, false)
 	clientOptions.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
-		if msg.Topic() == config.MQTTTopicControl && !controller.working {
+		if msg.Topic() == config.MQTTTopicControl && !controller.movingStatus {
 			switch string(msg.Payload()) {
 			case "O":
 				controller.OpenDoor()
@@ -118,25 +115,30 @@ func NewGarageController(config GarageControllerConfig, log *logging.Logger) *Ga
 		controller.log.Fatalf("Unable to subscribe to control topic '%s': %v", config.MQTTTopicControl, token.Error())
 	}
 
-	controller.currentDoorState = controller.readState()
-	controller.log.Infof("Door currently %v", controller.currentDoorState)
+	controller.setState(controller.readState())
 
 	return &controller
 }
 
-// readState pulls the status of the garage door unless the door is currently moving.  Then we return the current status
+// readState pulls the status of the garage door unless the door is currently movingStatus.  Then we return the current status
 func (controller *GarageController) readState() DoorState {
-	if controller.working {
+	if controller.movingStatus {
 		return controller.currentDoorState
 	}
-	if rpio.ReadPin(controller.pinStatus) == rpio.Low {
-		return DoorClosed
-	} else {
+
+	if rpio.ReadPin(controller.pinStatus) == rpio.High {
 		return DoorOpen
 	}
+
+	return DoorClosed
 }
 
-func (controller *GarageController) publishState() {
+func (controller *GarageController) setState(newState DoorState) {
+	if controller.currentDoorState == newState {
+		return
+	}
+	controller.log.Infof("Door state from %v to %v", controller.currentDoorState, newState)
+	controller.currentDoorState = newState
 	switch controller.currentDoorState {
 	case DoorOpen:
 		controller.client.Publish(controller.config.MQTTTopicStatus, 0, true, "O")
@@ -151,62 +153,67 @@ func (controller *GarageController) publishState() {
 
 func (controller *GarageController) toggleDoor() {
 	controller.log.Debug("Toggling door")
-	rpio.WritePin(controller.pinStatus, rpio.High)
+	rpio.WritePin(controller.pinControl, rpio.High)
 	time.Sleep(250 * time.Millisecond)
-	rpio.WritePin(controller.pinStatus, rpio.Low)
+	rpio.WritePin(controller.pinControl, rpio.Low)
 }
 
 // OpenDoor opens the door
 func (controller *GarageController) OpenDoor() {
+	if controller.currentDoorState != DoorClosed {
+		return
+	}
 	controller.log.Info("Opening Door")
-	controller.working = true
-	controller.currentDoorState = DoorOpening
+	controller.movingStatus = true
+	controller.setState(DoorOpening)
 	controller.toggleDoor()
 	go func(ch chan<- bool) {
 		time.Sleep(time.Duration(controller.config.TravelDelay) * time.Second)
 		ch <- false
-	}(controller.workingChannel)
+	}(controller.movingStatusChannel)
 }
 
 // CloseDoor closes the door
 func (controller *GarageController) CloseDoor() {
+	if controller.currentDoorState != DoorOpen {
+		return
+	}
 	controller.log.Info("Closing Door")
-	controller.working = true
-	controller.currentDoorState = DoorClosing
+	controller.movingStatus = true
+	controller.setState(DoorClosing)
 	controller.toggleDoor()
 	go func(ch chan<- bool) {
 		time.Sleep(time.Duration(controller.config.TravelDelay) * time.Second)
 		ch <- false
-	}(controller.workingChannel)
+	}(controller.movingStatusChannel)
 }
 
 // Run invoke the main controller loop
 func (controller *GarageController) Run() {
-	// periodically read door status
-	go func(ch chan<- DoorState) {
-		for {
-			time.Sleep(500 * time.Millisecond)
-			ch <- controller.readState()
-		}
-	}(controller.statusChannel)
 
 	// main loop
 	for {
+
+		// if the door isn't moving, check the status
+		if !controller.movingStatus {
+			controller.setState(controller.readState())
+		}
+
 		select {
-		case newState := <-controller.statusChannel:
-			if newState != controller.currentDoorState && !controller.working {
-				controller.log.Infof("Changing state from %v to %v", controller.currentDoorState, newState)
-				controller.currentDoorState = newState
-				controller.publishState()
-			}
-		case newWorking := <-controller.workingChannel:
-			controller.working = newWorking
+		case moving := <-controller.movingStatusChannel:
+			controller.movingStatus = moving
 		default:
+			time.Sleep(50 * time.Millisecond)
 		}
 	}
 }
 
 func main() {
+	if err := rpio.Open(); err != nil {
+		panic(err)
+	}
+	defer rpio.Close()
+
 	log := logging.MustGetLogger("loader")
 
 	configPath := flag.String("c", "controller.config", "Controller configuration file")
